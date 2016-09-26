6c21f39354626f09532757196ec188747973e353
Ernesto Perez <ernespn@gmail.com>
2016-09-26 23:23:02 +0100

    Add headers to allow cross origin

--- CalculatorService/CalculatorModule.cs
+++ CalculatorService/CalculatorModule.cs
@@ -12,7 +12,11 @@ namespace CalculatorServices
                 int x = parameters.x;
                 int y = parameters.y;
 
-                return Response.AsJson(new Calculation { result = x + y, from = "C# services" });
+                var response = Response.AsJson(new Calculation { result = x + y, from = "C# services" });
+                response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+                response.Headers.Add("Access-Control-Allow-Methods", "GET, OPTIONS");
+                response.Headers.Add("Access-Control-Allow-Origin", "*");
+                return response;
             };
         }
     }
