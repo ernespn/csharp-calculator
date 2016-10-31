ef54d18e1de5993b9d0e2fbde887f45905b9c2b6
Ernesto Perez <ernespn@gmail.com>
2016-10-31 19:59:07 +0000

    add cross origin to the todo service

--- TodoService/TodoModule.cs
+++ TodoService/TodoModule.cs
@@ -16,28 +16,38 @@ namespace TodoService
 
             Get["/todo"] = _ =>
             {
-                return Response.AsJson(_todoRepository.GetAll().ToList());
+                var response = Response.AsJson(_todoRepository.GetAll().ToList());
+                return CrossOrigin(response);
             };
 
             Post["/todo"] = _ =>
             {
                 var x = this.Bind<Todo>();
                 _todoRepository.AddOrUpdateTodo(x);
-                return Response.AsJson(_todoRepository.GetAll().ToList().Take(10));
+                var response =  Response.AsJson(_todoRepository.GetAll().ToList().Take(10));
+                return CrossOrigin(response);
             };
 
             Delete["/todo/{id}"] = parameters =>
             {
                 _todoRepository.DeleteTodo(parameters.id);
-                return Response.AsJson(_todoRepository.GetAll().ToList().Take(10));
+                var response = Response.AsJson(_todoRepository.GetAll().ToList().Take(10));
+                return CrossOrigin(response);
             };
 
             Get["/todo/{id}"] = parameters =>
             {
                 Todo todo = _todoRepository.GetById(parameters.id);
-                return Response.AsJson(todo);
+                var response = Response.AsJson(todo);
+                return CrossOrigin(response);
             };
         }
 
+        private Response CrossOrigin(Response response) {
+            response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
+        }
     }
 }
